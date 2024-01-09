0cab3d9d5f615004d7dcc48c511f26521be9736c
Jafin Jahfar <jafinjahfar@gmail.com>
2024-01-09 22:07:24 +0530

    feat(linked list): created linked list class and various LL methods

---
+++ src/App.java
@@ -0,0 +1,58 @@
+import packages.LinkedList.LinkedList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // initialised LL with value 1
+        LinkedList LL = new LinkedList(1);
+
+        LL.append(2);
+
+        System.out.println(LL.getLastValue());
+        LL.removeLast();
+        System.out.println(LL.getLastValue());
+        LL.removeLast();
+
+        LL.append(3);
+        LL.append(5);
+        LL.append(9);
+
+        // final list
+        System.out.println("Final Linked List items: ");
+        LL.printList();
+
+
+        System.out.println("After prepending 1: ");
+        //prepend 
+        LL.prepend(1);
+        LL.printList();
+
+        System.out.println("After removing first element 1: ");
+        LL.removeFirst();
+        LL.printList();
+
+        System.out.println("the Value at index 1 is: ");
+        System.out.println(LL.get(1).value);
+
+        System.out.println("The value at index 2 in the below linked list is to be changed: ");
+        LL.printList();
+        System.out.println("successfully updated - " + LL.set(2, 7));
+        System.out.println("The new list after updating the value at index 2 to 7 is as follows:");
+        LL.printList();
+
+        System.out.println("inserting 1 at 0th index is successfull: " + LL.insert(0, 1));
+        System.out.println("New list after insertion: ");
+        LL.printList();
+        
+        System.out.println("removing the element at index 2: " + LL.remove(2));
+        LL.printList();
+
+        System.out.println("adding 9 to end of the list: ");
+        LL.append(9);
+        LL.printList();
+
+        System.out.println("reversed array: ");
+        LL.reverse();
+        LL.printList();
+    }
+}
---
+++ src/packages/LinkedList/LinkedList.java
@@ -0,0 +1,195 @@
+package packages.LinkedList;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+  private Node head;
+  private Node tail;
+  private int length;
+
+  public class Node {
+    public int value;
+    Node next;
+
+    Node(int value) {
+      this.value = value;
+    }
+  }
+
+  public LinkedList(int value) {
+    Node newNode = new Node(value);
+    head = newNode;
+    tail = newNode;
+    length = 1;
+  }
+
+  public void printList() {
+      Node temp = head;
+      if (length == 0) System.out.println("Linked List is Empty");
+      while (temp != null) {
+        System.out.println(temp.value);
+        temp = temp.next;
+      }
+  }
+
+  public void getHead() {
+    System.out.println("Head" + this.head.value);
+  }
+
+  public void getTail() {
+    System.out.println("Tail" + this.tail.value);
+  }
+
+  public void getLength() {
+    System.out.println("Length" + this.length);
+  }
+
+  public int getLastValue() {
+    if (tail != null) return tail.value;
+    else {
+      throw new NoSuchElementException("The List is empty");
+    }
+  }
+
+  public void append(int value) {
+    Node newNode = new Node(value);
+
+    if (length == 0) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      tail.next = newNode;
+      tail = newNode;
+    }
+
+    length ++;
+  }
+
+  public void prepend(int value) {
+    Node newNode = new Node(value);
+    if (length == 0) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      newNode.next = head;
+      head = newNode;
+    }
+    length++;
+  }
+
+  public Node removeFirst() {
+    if (head == null) return null;
+    Node temp = head;
+
+    head = temp.next;
+    temp.next = null;
+
+    length--;
+    if (length == 0) {
+      tail = null;
+    }
+    return temp;
+  }
+
+  public Node removeLast() {
+    Node prev = head;
+    Node temp = head;
+
+    if (length == 0) return null;
+
+    while (temp.next != null) {
+      prev = temp;
+      temp = temp.next;
+    }
+
+    tail = prev;
+    tail.next = null;
+    length--;
+    
+    if (length == 0) {
+      head = null;
+      tail = null;
+    }
+
+    return temp;
+  }
+
+  public Node get(int index) {
+    if (index < 0 || index >= length) return null;
+
+    Node temp = head;
+    for (int i = 0; i < index; i++) {
+      temp = temp.next;
+    }
+
+    return temp;
+  }
+
+  public Boolean set(int index, int value) {
+    Node temp = get(index);
+
+    if (temp != null) {
+      temp.value = value;
+      return true;
+    }
+
+    return false;
+  }
+
+  public boolean insert(int index, int value) {
+    Node newNode = new Node(value);
+    Node temp = get(index - 1);
+
+    if (index < 0 || index > length) return false;
+    if (index == 0) {
+      prepend(value);
+      return true;
+    }else if (index == length) {
+      append(value);
+      return true;
+    }
+
+    newNode.next = temp.next;
+    temp.next = newNode;
+
+    length++;
+
+    return true;
+  }
+
+  public Node remove(int index) {
+    if (index < 0 || index >= length) return null;
+
+    if(index == 0) {
+      return removeFirst();
+    }
+    if (index == length - 1) {
+      return removeLast();
+    }
+
+    Node prev = get(index-1);
+    Node temp = prev.next;
+
+    prev.next = temp.next;
+    temp.next = null;
+
+    length --;
+    return temp;
+  }
+
+  public void reverse() {
+    Node temp = head;
+    head = tail;
+    tail = temp;
+
+    Node before = null;
+    Node after = temp.next;
+
+    for (int i = 0; i < length; i++) {
+      after = temp.next;
+      temp.next = before;
+      before = temp;
+      temp = after;
+    }
+  }
+}
