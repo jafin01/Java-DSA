038ad9747b81e3d0dc01a1a8b5955d11c9da5a32
Jafin Jahfar <jafinjahfar@gmail.com>
2024-01-15 00:42:55 +0530

    feat(stack): added stack and its methods

---
+++ src/Stack/Main.java
@@ -0,0 +1,28 @@
+package Stack;
+
+public class Main {
+  public static void main(String[] args) {
+    // initialising a new Stack with value 5
+    Stack myStack = new Stack(5);
+    myStack.printStack();
+
+    // adding a new value to the stack
+    System.out.println("Adding value 3 and 4 to the Stack");
+    myStack.push(4);
+    myStack.push(3);
+    myStack.printStack();
+
+    // removing an element from the stack
+    System.out.println("Removing the top element from the list");
+    myStack.pop();
+    myStack.printStack();
+
+    System.out.println("Removing the top element from the list");
+    myStack.pop();
+    myStack.printStack();
+
+    System.out.println("Removing the top element from the list");
+    myStack.pop();
+    myStack.printStack();
+  }
+}
---
+++ src/Stack/Stack.java
@@ -0,0 +1,69 @@
+package Stack;
+
+public class Stack {
+  private Node top;
+  private int height;
+
+  public class Node {
+    int value;
+    Node next;
+
+    Node (int value) {
+      this.value = value;
+    }
+  }
+  
+  public Stack (int value) {
+    Node newNode = new Node(value);
+    top = newNode;
+    height++;
+  }
+
+  public void printStack() {
+    if (top == null) {
+      System.out.println("Stack is empty");
+      return;
+    }
+
+    Node temp = top;
+
+    while (temp != null) {
+      System.out.println(temp.value + " -> ");
+      temp = temp.next;
+    }
+  }
+
+  public void getTop() {
+    System.out.println("Top = " + top.value);
+  }
+
+  public void getHeight() {
+    System.out.println("Height of the stack = " + height);
+  }
+
+  public void push(int value) {
+    Node newNode = new Node(value);
+
+    if (top == null) {
+      top = newNode;
+    }else {
+      newNode.next = top;
+      top = newNode;
+    }
+
+    height++;
+  }
+
+  public void pop() {
+    if (top == null) return;
+
+    if (height == 1) top = null;
+    else {
+      Node temp = top;
+      top = top.next;
+      temp.next = null;
+    }
+
+    height--;
+  }
+}
