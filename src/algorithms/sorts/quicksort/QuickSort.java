6234195b12b9f88f6af183ef3634ed075a012b15
Jafin Jahfar <jafinjahfar@gmail.com>
2024-01-26 15:54:48 +0530

    feat(sorting algorithms): added quick sort algorithm

--- src/algorithms/sorts/quicksort/QuickSort.java
+++ src/algorithms/sorts/quicksort/QuickSort.java
@@ -12,7 +12,7 @@ public class QuickSort {
   public static int pivot(int[] array, int pivotIndex, int endIndex) {
     int swapIndex = pivotIndex;
 
-    for (int i = pivotIndex + 1; i < endIndex; i++) {
+    for (int i = pivotIndex + 1; i <= endIndex; i++) {
       if (array[i] < array[pivotIndex]) {
         swapIndex++;
         swap(array, swapIndex, i);
@@ -23,10 +23,26 @@ public class QuickSort {
     return swapIndex;
   }
 
+  public static void quickSort(int[] array, int left, int right) {
+    if (left < right) {
+      int pivotIndex = pivot(array, left, right);
+      quickSort(array, left, pivotIndex - 1);
+      quickSort(array, pivotIndex + 1, right);
+    }
+  }
+
+  public static void quickSort(int[] array) {
+    quickSort(array, 0, array.length - 1);
+  }
+
   public static void main (String[] args) {
     int[] array = new int[]{4, 6, 1, 7, 3, 2, 5};
 
-    System.out.println(pivot(array, 0, array.length - 1));
+    // System.out.println(pivot(array, 0, array.length - 1));
+
+    System.out.println(Arrays.toString(array));
+
+    quickSort(array);
 
     System.out.println(Arrays.toString(array));
 
